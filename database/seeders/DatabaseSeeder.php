b754689d3d600bc27842a599fe60415df0bdca7a
ridwancodevioso <ridwan@codevioso.com>
2025-07-18 12:26:12 +0600

    code update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+        // Uncomment the following line to seed the LocationTypeSeeder
+         $this->call(LocationTypeSeeder::class);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
