24b2a9a8eb7609f2a396b11b6b78149979aa6b03
ridwancodevioso <ridwan@codevioso.com>
2025-07-18 12:15:11 +0600

    deleted at fixed

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -4,12 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 
 class Location extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [
         'company_id',
@@ -25,7 +24,6 @@ class Location extends Model
     ];
 
     protected $casts = [
-        'deleted_at' => 'datetime',
     ];
 
     protected $appends = [
